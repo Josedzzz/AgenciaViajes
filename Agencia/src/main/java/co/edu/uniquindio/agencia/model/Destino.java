8d3cfc8112494ac00f99789cad85e2b1a703f25d
jokizz <josedavidamayar@gmail.com>
2023-09-20 02:07:08 -0500

    Se crea el atributo lista de imagenes para la clase destino

--- Agencia/src/main/java/co/edu/uniquindio/agencia/model/Destino.java
+++ Agencia/src/main/java/co/edu/uniquindio/agencia/model/Destino.java
@@ -3,6 +3,8 @@ package co.edu.uniquindio.agencia.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @Getter
 @Setter
 public class Destino {
@@ -10,12 +12,14 @@ public class Destino {
     private String nombre;
     private String ciudad;
     private String descripcion;
+    private ArrayList<String> listaImagenes;
     private TipoClima tipoClima;
 
     public Destino(String nombre, String ciudad, String descripcion, TipoClima tipoClima) {
         this.nombre = nombre;
         this.ciudad = ciudad;
         this.descripcion = descripcion;
+        this.listaImagenes = new ArrayList<String>();
         this.tipoClima = tipoClima;
     }
 
